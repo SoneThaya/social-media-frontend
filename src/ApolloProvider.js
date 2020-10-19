b091dc43d17e3ce20cebf41fe18232bdbdc042dd
Sone <sthaya@gmail.com>
2020-10-18 17:04:03 -0700

    install graphql to fix error

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,23 +4,23 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { setContext } from "apollo-link-context";
+//import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "http://localhost:8000",
 });
 
-const authLink = setContext(() => {
-  const token = localStorage.getItem("jwtToken");
-  return {
-    headers: {
-      Authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+// const authLink = setContext(() => {
+//   const token = localStorage.getItem("jwtToken");
+//   return {
+//     headers: {
+//       Authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
