3eb7da9dd5718040de467fc45f409d8ba5754c75
Sone <sthaya@gmail.com>
2020-10-18 19:37:40 -0700

    created auth componet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { Grid, Transition } from "semantic-ui-react";
 
-import { AuthContext } from "../context/auth";
+import { AuthContext } from "../context/auth"
 import PostCard from "../components/PostCard";
 import PostForm from "../components/PostForm";
 import { FETCH_POSTS_QUERY } from "../util/graphql";
